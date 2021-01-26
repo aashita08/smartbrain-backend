ea8959c81fd2a23d7504cef6286a16321f601335
Aashita8 <tiwari.aashita@gmail.com>
2021-01-26 21:27:17 +0530

    URL update-2

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ const image=require('./controllers/image')
 const db=knex({
     client:'pg',
     connection:{
-        connectionString:process.env.DATABASE_URL,
-        ssl:true
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
         // host:'127.0.0.1',
         // user:'postgres',
         // password:'aashita12345',
