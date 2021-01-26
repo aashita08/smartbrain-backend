145fb63f59d207fca81ad58dea9d0433c72ac16e
Aashita8 <tiwari.aashita@gmail.com>
2021-01-26 20:44:12 +0530

    updated db URL

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ const image=require('./controllers/image')
 const db=knex({
     client:'pg',
     connection:{
-        host:'127.0.0.1',
-        user:'postgres',
-        password:'aashita12345',
-        database:'smart-brain' 
+        connectionString:process.env.DATABASE_URL,
+        ssl:true
+        // host:'127.0.0.1',
+        // user:'postgres',
+        // password:'aashita12345',
+        // database:'smart-brain' 
     }
 })
 
